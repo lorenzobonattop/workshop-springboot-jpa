2d9024b8b5b3b9edf20961a3f07db3b11187d40c
Lorenzo Bonatto <lorenzobonattop@gmail.com>
2025-02-21 12:29:17 -0300

    User delete

--- src/main/java/com/projeto/course/resources/UserResource.java
+++ src/main/java/com/projeto/course/resources/UserResource.java
@@ -33,6 +33,12 @@ public class UserResource {
     public ResponseEntity<User> insert(@RequestBody User obj){
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-        return ResponseEntity.created(uri).body(obj);   
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
